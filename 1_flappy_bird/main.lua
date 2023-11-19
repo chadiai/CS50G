76d6117ab5f70f545f93b6b1cd7a6d7e2c05b5f5
chadiai <chadii@outlook.be>
2023-11-19 21:19:43 +0100

    jump control

--- 1_flappy_bird/main.lua
+++ 1_flappy_bird/main.lua
@@ -30,10 +30,27 @@ function love.load()
         fullscreen = false,
         resizable = true
     })  
+
+    love.keyboard.keysPressed = {}
 end
 
 function love.resize(w,h)
     push:resize(w,h)
+end
+
+function love.keypressed(key)
+    love.keyboard.keysPressed[key] = true
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+function love.keyboard.wasPressed(key)
+    if love.keyboard.keysPressed[key] then
+        return true
+    else
+        return false
+    end
 end
 
 function love.update(dt)
@@ -43,11 +60,8 @@ function love.update(dt)
         % VIRTUAL_WIDTH
 
     bird:update(dt)
-end
-function love.keypressed(key)
-    if key == 'escape' then
-        love.event.quit()
-    end
+
+    love.keyboard.keysPressed = {}
 end
 
 function love.draw()
